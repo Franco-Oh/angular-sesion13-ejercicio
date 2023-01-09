ce831fe4a677d925cb6c998b6982bdc1fbe6fc30
Franco Ojeda <fojedavideos@gmail.com>
2023-01-09 15:36:17 -0500

    implementacion de json pipe

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,6 +12,44 @@ export class HomeComponent implements OnInit {
   monedas:string[]=["USD","EUR","S/ "];
   seleccionado!:string;
   
+  // Datos predefinidos para el JSON
+  personas = [{
+    "Apellido": "Arcila",
+    "Casado": false,
+    "Dirección": "Calle 35",
+    "Nombre": "Diego",
+    "Telefono": 3859720
+  },{
+    "Apellido": "Bueno",
+    "Casado": false,
+    "Dirección": "CR #53",
+    "Nombre": "Kevin",
+    "Telefono": 31485579954
+  },{
+    "Apellido": "Palomino",
+    "Casado": false,
+    "Dirección": "CR #52",
+    "Nombre": "Natalia",
+    "Telefono": 32255945555
+  }
+  ]
+
+  //Agregar datos para JSON
+  apell!:string;
+  casado = false;
+  dir!:string;
+  nom!:string;
+  telef!:number;
+
+  agregar(){
+    this.personas.push({
+      Apellido: this.apell,
+      Casado: this.casado,
+      Dirección: this.dir,
+      Nombre: this.nom,
+      Telefono: this.telef
+    })
+    }
 
   constructor() { }
 
