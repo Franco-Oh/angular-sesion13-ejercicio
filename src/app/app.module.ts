fff1556722151e7f33089f2be770663518094574
Franco Ojeda <fojedavideos@gmail.com>
2023-01-09 09:34:51 -0500

    Creacion de carpeta y elementos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PagesModule } from './pages/pages.module';
+import { PipesTestingPipe } from './pipes-testing.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesTestingPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
